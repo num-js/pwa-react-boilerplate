c2ea103c7c42ca88a23a672727166eadf6da3944
Numan Ahmed <mdnmnahmed@gmail.com>
2021-08-21 20:20:56 +0530

    PWA Setup Configurations

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <>
+            <div align="center">
+                <h1>PWA-React-Boilerplate</h1>
+                <p>by NumDev</p>
+            </div>
+        </>
+    );
+}
+
+export default App;
